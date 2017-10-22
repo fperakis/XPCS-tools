ca0482c2682a5cc0171c14a72116434ff7fde10c
Jonas Sellberg <jonas.sellberg@naturalcycles.com>
2017-10-22 18:50:53 +0200

    removed redundant parameters.py, added .gitignore, added package build with setup.py

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# xpcs-tools package init
+# import submodules here:
+#__all__ = ["submodule"]
+# import Classes here:
+from xpcstools import fit_tools
+from xpcstools import mask_tools
+from xpcstools import temporal_autocorrelation
+from xpcstools import parameters
